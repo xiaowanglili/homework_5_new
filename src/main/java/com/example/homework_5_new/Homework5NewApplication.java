a905e8a9eba026800cb87b0b635aaf09892092ae
wangruihan <3390006857@qq.com>
2025-12-29 22:09:58 +0800

    Initial commit

---
+++ src/main/java/com/example/homework_5_new/Homework5NewApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework_5_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework5NewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework5NewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework_5_new/Homework5NewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_5_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework5NewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
